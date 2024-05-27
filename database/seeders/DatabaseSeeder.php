9ac83212bb54b6540efdd92b05d9b87ded16d9be
Player001 <joshababon@gmail.com>
2024-05-27 11:57:39 +0800

    Revert "I'm doing my part"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,25 +2,22 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(RoleSeeder::class);
-        $this->call(AdminSeeder::class);
-        $this->call(MembershipSeeder::class);
-        $this->call(GymClassSeeder::class);
-        $this->call(CustomerSeeder::class);
-        $this->call(CustMemSeeder::class);
-        $this->call(EmployeeSeeder::class);
+        // User::factory(10)->create();
 
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
